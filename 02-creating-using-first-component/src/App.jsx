89d09197f65718c8d29087113f6aad934487a47c
SohamBhatt99 <sohambhatt139@gmail.com>
2024-11-16 16:44:41 +0530

    practiceing array with calling name using random function

--- 02-creating-using-first-component/src/App.jsx
+++ 02-creating-using-first-component/src/App.jsx
@@ -1,20 +1,25 @@
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
+const NameDetails =  ['soham', 'paresh', 'sonal']; //created one array of name details
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1)); // random number function to call name in every refresh
 }
 
+function Testing () {
+const description = NameDetails[genRandomInt(3)]; // dont use [] brackets in this use () bracket for function 
+  return (
+    <header>
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>{description} Essentials </h1>
+    <p> React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  )
+}
 function App() {
   return (
     <div>
-      <Header />
+<Testing/>
       <main>
         <h2>Time to get started!</h2>
       </main>

555af5ae951bc165b116979242704f5c489ab845
SohamBhatt99 <sohambhatt139@gmail.com>
2024-11-16 17:31:21 +0530

    final

--- 02-creating-using-first-component/src/App.jsx
+++ 02-creating-using-first-component/src/App.jsx
@@ -1,48 +1,21 @@
-import reactimg from "./assets/react-core-concepts.png"
-import reactimg2 from "./assets/components.png"
 
-const NameDetails =  ['soham', 'paresh', 'sonal']; //created one array of name details
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1)); // random number function to call name in every refresh
-}
-
-function Testing () {
-const description = NameDetails[genRandomInt(3)]; // dont use [] brackets in this use () bracket for function 
+function Header() {
   return (
     <header>
-    <img src= {reactimg} alt="Stylized atom" />
-    <h1>{description} Essentials </h1>
-    <p> React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  )
-}
-function CoreConcept (props) {
-  return (
-    <li>
-      <img src={props.image} alt="" />
-      <h3>{props.title}</h3>
-      <p>{props.descriptions}</p>
-    </li>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
-
 }
+
 function App() {
   return (
     <div>
-<Testing/>
-<main id ="core-concepts">
-<CoreConcept
-image = {reactimg2}
-title ="Hello soham here"
-descriptions = "hello soham here i am developer learning react js and currently practicing"
-
-/>
-<coreconcepts/>
-<coreconcepts/>
-</main>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
